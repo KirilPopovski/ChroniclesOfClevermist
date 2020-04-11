46079c6d75b02bd95b28f79d2491e47348247ad8
KirilPopovski <48929770+KirilPopovski@users.noreply.github.com>
2020-04-11 17:56:26 +0300

    Add Contacts controller and views

---
+++ Web/ChroniclesOfClevermist.Web/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿namespace ChroniclesOfClevermist.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController : Controller
+    {
+        public IActionResult About()
+        {
+            return this.View();
+        }
+
+        public IActionResult History()
+        {
+            return this.View();
+        }
+
+        public IActionResult SiteMap()
+        {
+            return this.View();
+        }
+    }
+}
